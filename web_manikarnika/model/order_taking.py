d729f99d56dcd55d97b9fac2bff9761b500ebbff
Hemangini Patel <h.patel.serpentcs@gmail.com>
2016-12-24 14:50:44 +0530

    [IMP]Recreate Manikarnika and Grains Interface.

--- web_manikarnika/model/order_taking.py
+++ web_manikarnika/model/order_taking.py
@@ -29,30 +29,81 @@ class order_tackinig(models.Model):
     _inherit = 'order.tacking'
 
     @api.model
-    def order_tracking_create(self):
-        comp_obj = self.env['res.company']
+    def order_taking_create(self):
+        if self._context is None:
+            self._context = {}
+        if self._context:
+            MK_lst = []
+            GR_lst = []
+            prod_obj = self.env['product.product']
+            order_dt = date.today().strftime('%Y-%m-%d')
+            valu_dic = self._context.values()
+            orders = self.search([('partner_id.id','=',
+                                  valu_dic[0].keys()[0]),
+                                 ('order_date','=', order_dt)])
+            if orders:
+                for order in orders:
+                    if self._context.keys()[0] == 'manik':
+                        if order.morder_tacking_line_ids:
+                            for mnk_lst in order.morder_tacking_line_ids:
+                                for p in valu_dic[0].values()[0]:
+                                    if (int(mnk_lst.product_id.id) == int(p['product_id'])):
+                                        mnk_lst.write({'order_qty': p['order_qty']})
+                            return order.id
+                        for products in valu_dic[0].values():
+                            MK_lst = self.get_order_taking_lines(products)
+                        order.morder_tacking_line_ids = MK_lst
+                        return order.id
+                    if self._context.keys()[0] == 'grain':
+                        if order.gorder_tacking_line_ids:
+                            for grn_lst in order.gorder_tacking_line_ids:
+                                for p in valu_dic[0].values()[0]:
+                                    if (int(grn_lst.product_id.id) == int(p['product_id'])):
+                                        grn_lst.write({'order_qty': p['order_qty']})
+                            return order.id
+                        for products in valu_dic[0].values():
+                            GR_lst = self.get_order_taking_lines(products)
+                        order.gorder_tacking_line_ids = GR_lst
+                        return order.id
+            if self._context.keys()[0] == 'manik':
+                MK_lst = self.get_order_taking_lines(valu_dic[0].values()[0])
+                if MK_lst:
+                    order_taking_id = self.create({'partner_id': valu_dic[0].keys()[0],
+                                                   'order_date': order_dt,
+                                                   'state': 'draft' })
+                    order_taking_id.morder_tacking_line_ids = MK_lst
+                    return order_taking_id.id
+            if self._context.keys()[0] == 'grain':
+                GR_lst = self.get_order_taking_lines(valu_dic[0].values()[0])
+                order_taking_id = self.create({'partner_id': valu_dic[0].keys()[0],
+                                               'order_date': order_dt,
+                                               'state': 'draft' })
+                order_taking_id.gorder_tacking_line_ids = GR_lst
+                return order_taking_id.id
+
+    @api.multi
+    def get_order_taking_lines(self, products):
         prod_obj = self.env['product.product']
-        part_obj = self.env['res.partner']
-        partners = part_obj.search([('customer', '=', 'True')])
-        date_today = date.today().strftime('%Y-%m-%d')
-        comp_MK = comp_obj.search([('comp_code','=','MK')])
-        comp_GR = comp_obj.search([('comp_code','=','GR')])
-        MK_products = prod_obj.search([('company_id','in', comp_MK.ids)])
-        GR_products = prod_obj.search([('company_id','in', comp_GR.ids)])
-        MK_lst = []
-        GR_lst = []
-        if MK_products:
-            MK_lst = self.get_order_tarcking_lines(MK_products)
-        if GR_products:
-            GR_lst = self.get_order_tarcking_lines(GR_products)
-        if partners:
-            for partner in partners:
-                o_t_id = self.create({'partner_id': partner.id,
-                                      'order_date': date_today,
-                                      'state': 'draft' })
-                o_t_id.morder_tacking_line_ids = MK_lst
-                o_t_id.gorder_tacking_line_ids = GR_lst
-        return {}
+        order_track_lines_lst = []
+        order_dt = date.today().strftime('%Y-%m-%d')
+        if self.order_date:
+            order_dt = datetime.strptime(self.order_date, "%Y-%m-%d")
+            order_dt = order_dt + timedelta (days=1)
+            order_dt = order_dt.strftime("%Y-%m-%d")
+        if products:
+            sr_no = 1
+            for product in products:
+                prod = prod_obj.search([('id','=',
+                                                product['product_id'])])
+                order_track_lines_lst.append((0,0,{'serial_no': sr_no,
+                           'product_id': prod.id,
+                           'qty_aval': prod.qty_available or 0.0,
+                           'default_order_qty': prod.default_qty or 0.0,
+                           'order_price': prod.lst_price or 0.0,
+                           'order_qty': product['order_qty'],
+                           'order_date_line': order_dt or False}))
+                sr_no += 1
+        return order_track_lines_lst
 
 
 class vehicle_allocation(models.Model):
@@ -79,7 +130,6 @@ class vehicle_allocation(models.Model):
                 vehicle_id = self.create({'vehicle_id': vehicle.id,
                                           'driver_id': vehicle.driver_id.id,
                                           'order_date': date_today})
-                print "\n\n::::vehicle_id",vehicle_id.vehicle_allocation_line_ids
                 vehicle_id.vehicle_allocation_line_ids = MK_GR_lst
         return {}
 
--- web_manikarnika/static/src/js/manikarnika.js
+++ web_manikarnika/static/src/js/manikarnika.js
@@ -62,51 +62,7 @@ openerp.web_manikarnika = function(instance) {
     	    });
     	});
     });
-    
-    instance.web.client_actions.add('manikarnika.homepage', 'instance.web_manikarnika.action');
-    instance.web_manikarnika.action = instance.web.Widget.extend({
-    	events: {
-    		'keyup input': 'input_qty_keyup',
-    		'click #edit': 'input_edit_click',
-        },
-        template: "ManikarnikaTemp",
-        init: function(parent, name) {
-            this._super(parent);
-            var self = this
-            this.orders = order_dic
-            $.each(product_list, function(i, el){
-                if($.inArray(el, product_id_list) === -1) product_id_list.push(el);
-            });
-            this.product = product_id_list
-            this.default_qty = manik_list
-            this.item_dic = item_dic
-            if (check == 0 ){
-        		var model = new instance.web.Model("order.tacking");
-            	model.call('order_tracking_create',{context: new instance.web.CompoundContext()}).then(function(result){
-            		location.reload(true)
-            	});
-        		
-            }
-        },
-        start: function() {
-        },
-        input_edit_click : function(ev)
-        {
-        	var $action = $(ev.currentTarget);
-        	$action.parent().parent().find('input').attr("readonly", false)
-        },
-        
-        input_qty_keyup: function(ev) {
-        	var self = this
-        	var $action = $(ev.currentTarget);
-            var id = parseInt($action.attr('id'));
-        	var model = $action.attr('model');
-        	order_qty = parseFloat(ev.target.value)
-        	self.table_master_dataset = new instance.web.DataSetSearch(self, model, {}, []);
-        	self.table_master_dataset.write(id, {'order_qty': parseFloat(order_qty)})
-        },
-    });
-    
+
 //    ************ Grains **************
     
     var grain_order_dic = {}
@@ -151,49 +107,6 @@ openerp.web_manikarnika = function(instance) {
     	    	grain_order_dic[grain_r.partner_id[1]] = grain_val_list
     	    });
     	});
-    });
-    
-    instance.web.client_actions.add('grains.homepage', 'instance.web_manikarnika.grain_action');
-    instance.web_manikarnika.grain_action = instance.web.Widget.extend({
-    	events: {
-    		'keyup input': 'grain_input_qty_keyup',
-    		'click #edit': 'input_edit_click',
-        },
-        template: "GrainsTemp",
-        init: function(parent, name) {
-            this._super(parent);
-            var self = this
-            this.grain_orders = grain_order_dic
-            $.each(grain_product_list, function(j, element){
-                if($.inArray(element, grain_product_id_list) === -1) grain_product_id_list.push(element);
-            });
-            this.grain_product = grain_product_id_list
-            this.grains_default_qty = grain_list
-            this.grain_item_dic = grain_item_dic
-            if (grain_check == 0 )
-            {
-        		var model = new instance.web.Model("order.tacking");
-            	model.call('order_tracking_create',{context: new instance.web.CompoundContext()}).then(function(result){
-            		location.reload(true)
-            	});
-            }
-        },
-        start: function() {
-        },
-        input_edit_click : function(ev)
-        {
-        	var $action = $(ev.currentTarget);
-        	$action.parent().parent().find('input').attr("readonly", false)
-        },
-        grain_input_qty_keyup: function(ev) {
-        	var self = this
-        	var $action = $(ev.currentTarget);
-            var id = parseInt($action.attr('id'));
-        	var model = $action.attr('model');
-        	order_qty = parseFloat(ev.target.value)
-        	self.grain_master_dataset = new instance.web.DataSetSearch(self, model, {}, []);
-        	self.grain_master_dataset.write(id, {'order_qty': parseFloat(order_qty)})
-        },
     });
     
 //    ************************** Vehicle Allocation *****************************
@@ -254,13 +167,13 @@ openerp.web_manikarnika = function(instance) {
             this._super(parent);
             var self = this
             this.vehicles = vehicle_dic
-            if ( vehicle_check == 0 )
+            /*if ( vehicle_check == 0 )
             {
         		var model = new instance.web.Model("vehicle.allocation");
             	model.call('vehicle_allocation_create',{context: new instance.web.CompoundContext()}).then(function(result){
             		location.reload(true)
             	});
-            }
+            }*/
             if(flag == 0){
             	flag = 1
             	var keys = $.map( vehicle_product_total, function( value, key ) {
@@ -471,32 +384,76 @@ openerp.web_manikarnika = function(instance) {
 //    ************************************************Order Taking *************************************
     
 //    ********************************* Manikarnika Order ***********************************
-    
-    manik_product_lst = []
-    manik_cust_lst = []
-    manik_order_dict = {}
+
+    var manik_product_lst = []
+    var manik_cust_lst = []
+    var manik_order_dict = {}
+    var item_total_dic = {}
+    var item_total_dic2 = {}
     self.manik_comp_dataset = new instance.web.DataSetSearch(self, 'res.company', {}, [['comp_code', '=' , 'MK']]);
     self.manik_comp_dataset.read_slice([], {'domain': []}).done(function(records_com) {
     	_.each(records_com, function(r){
     		self.manik_product_dataset = new instance.web.DataSetSearch(self, 'product.product', {}, [['company_id', '=' , r.id]]);
     	    self.manik_product_dataset.read_slice([], {'domain': []}).done(function(records_pro) {
-    	    	manik_product_lst = []
     	    	_.each(records_pro, function(p){
-    	    		manik_product_lst.push({'product_nm': p.name, 'product_id': p.id, 'default_qty': p.default_qty})
+    	    		manik_product_lst.push({'product_nm': p.name, 'product_id': p.id,
+    	    								'default_qty': p.default_qty, 'order_qty': 0.0, 'manik_qty': 0.0})
     	    	})
     	    });
     	})
     });
-    
-    
 
     self.manik_customer_dataset = new instance.web.DataSetSearch(self, 'res.partner', {}, [['customer', '=' , 'True']])
     self.manik_customer_dataset.read_slice([], {'domain': []}).done(function(records_cus) {
-    	console.log("manik_product_lst",manik_product_lst)
     	_.each(records_cus, function(c){
-    		manik_cust_lst = []
-    		manik_cust_lst.push({'customer_id': c.id, 'product_lst': manik_product_lst})
-    		manik_order_dict[c.name] = manik_cust_lst
+    		self.manik_ord_dataset = new instance.web.DataSetSearch(self, 'order.tacking', {}, [['order_date', '=' , curr_date],['partner_id', '=', c.id]])
+    	    self.manik_ord_dataset.read_slice([], {'domain': []}).done(function(records_ord) {
+    	    	if (records_ord.length > 0){
+    	    		_.each(records_ord, function(o){
+    	    			product_m_list = []
+    	    			self.manik_line_dataset = new instance.web.DataSetSearch(self, 'morder.tacking.line', {}, [['id','in', o.morder_tacking_line_ids]]);
+    	        	    self.manik_line_dataset.read_slice([], {'domain': []}).done(function(line_rec) {
+    	        	    	if( line_rec.length > 0){
+	    	        	    	manik_qty = 0
+	    	        	    	_.each(line_rec, function(product){
+	    	        	    		product_m_list.push({'product_nm': product.product_id[1],
+	    	        	    						   'product_id': product.product_id[0],
+	    	        	    						   'default_qty': product.default_qty,
+	    	        	    						   'order_qty': product.order_qty})
+		    						manik_qty = manik_qty + product.order_qty
+		    						if ( product.product_id[0] in item_total_dic2)
+		    				  		 {
+		    					  		 total = (item_total_dic2[product.product_id[0]] + product.order_qty)
+		    					  		 item_total_dic[product.product_id[1]] = [{'qty': total}]
+		    					  		 item_total_dic2[product.product_id[0]] = total
+		    				  		 }
+		    					  	 else
+		    					  	 {
+		    					  		item_total_dic[product.product_id[1]] = [{'qty': product.order_qty}]
+		    					  		item_total_dic2[product.product_id[0]] = product.order_qty
+		    					  	 }
+	    	        	    	})
+	    	        	    	manik_cust_lst = []
+		        	    		manik_cust_lst.push({'customer_id': c.id, 'product_lst': product_m_list,
+		        	    							 'manik_qty': manik_qty})
+		        	    		manik_order_dict[c.name] = manik_cust_lst
+		        	    		product_m_list = []
+    	        	    	}
+    	        	    	else{
+    	        	    		manik_cust_lst = []
+    	        	    		manik_cust_lst.push({'customer_id': c.id, 'product_lst': manik_product_lst})
+    	        	    		manik_order_dict[c.name] = manik_cust_lst
+    	        	    	}
+    	        	    })
+    	    		})
+    	    	}
+    	    	else{
+    	    		manik_cust_lst = []
+    	    		manik_cust_lst.push({'customer_id': c.id, 'product_lst': manik_product_lst})
+    	    		manik_order_dict[c.name] = manik_cust_lst
+    	    	}
+    	    })
+    		
     	})
     });
 
@@ -512,6 +469,7 @@ openerp.web_manikarnika = function(instance) {
             var self = this;
             this.manik_product_lst = manik_product_lst;
             this.manik_order_dict = manik_order_dict
+            this.item_total = item_total_dic
         },
         start: function() {
         },
@@ -524,12 +482,186 @@ openerp.web_manikarnika = function(instance) {
         },
         input_save_click: function(ev)
         {
+        	ev.preventDefault();
         	var $action = $(ev.currentTarget);
+        	order_dic = {'manik': {}}
+        	view = {}
+        	list_pro = []
+        	$action.parent().parent().find('td').each(function(){
+        		if($(this).find("input")){
+        			if($(this).find("input").attr('id')){
+        				list_pro.push({'product_id': $(this).find("input").attr('id'),'order_qty':$(this).find("input").val()})
+        			}
+				}
+        	})
+        	view[$action.data('dic')] = list_pro
+        	order_dic['manik'] = view
+        	if(view){
+        		var model = new instance.web.Model("order.tacking");
+            	model.call('order_taking_create',{context: order_dic}).then(function(result){
+            		
+            	});
+        	}
         	$action.parent().parent().find('input').attr("readonly", 'readonly')
         	$action.css('visibility', 'hidden')
         	$action.parent().parent().find('#edit').css('visibility', 'visible')
         },
     });
+    
+//  ********************************* Grains Order ***********************************
+
+    var gr_product_lst = []
+    var gr_cust_lst = []
+    var gr_order_dict = {}
+    var gr_item_total_dic = {}
+    var gr_item_total_dic2 = {}
+    self.gr_comp_dataset = new instance.web.DataSetSearch(self, 'res.company', {}, [['comp_code', '=' , 'GR']]);
+    self.gr_comp_dataset.read_slice([], {'domain': []}).done(function(records_com) {
+    	_.each(records_com, function(r){
+    		self.gr_product_dataset = new instance.web.DataSetSearch(self, 'product.product', {}, [['company_id', '=' , r.id]]);
+    	    self.gr_product_dataset.read_slice([], {'domain': []}).done(function(records_pro) {
+    	    	_.each(records_pro, function(p){
+    	    		gr_product_lst.push({'product_nm': p.name, 'product_id': p.id,
+    	    							 'default_qty': p.default_qty, 'order_qty': 0.0, 'gr_qty': 0.0})
+    	    	})
+    	    });
+    	})
+    });
+
+    self.gr_customer_dataset = new instance.web.DataSetSearch(self, 'res.partner', {}, [['customer', '=' , 'True']])
+    self.gr_customer_dataset.read_slice([], {'domain': []}).done(function(records_cus) {
+    	_.each(records_cus, function(c){
+    		self.gr_ord_dataset = new instance.web.DataSetSearch(self, 'order.tacking', {}, [['order_date', '=' , curr_date],['partner_id', '=', c.id]])
+    	    self.gr_ord_dataset.read_slice([], {'domain': []}).done(function(records_ord) {
+    	    	if (records_ord.length > 0){
+    	    		_.each(records_ord, function(o){
+    	    			product_g_list = []
+    	    			self.gr_line_dataset = new instance.web.DataSetSearch(self, 'gorder.tacking.line', {}, [['id','in', o.gorder_tacking_line_ids]]);
+    	        	    self.gr_line_dataset.read_slice([], {'domain': []}).done(function(line_rec) {
+    	        	    	if( line_rec.length > 0){
+    	        	    		gr_qty = 0
+        	        	    	_.each(line_rec, function(product){
+        	        	    		product_g_list.push({'product_nm': product.product_id[1],
+        	        	    						   'product_id': product.product_id[0],
+        	        	    						   'default_qty': product.default_qty,
+        	        	    						   'order_qty': product.order_qty})
+    	    						gr_qty = gr_qty + product.order_qty
+    	    						if ( product.product_id[0] in gr_item_total_dic2)
+    	    				  		 {
+    	    					  		 total = (gr_item_total_dic2[product.product_id[0]] + product.order_qty)
+    	    					  		 gr_item_total_dic[product.product_id[1]] = [{'qty': total}]
+    	    					  		 gr_item_total_dic2[product.product_id[0]] = total
+    	    				  		 }
+    	    					  	 else
+    	    					  	 {
+    	    					  		gr_item_total_dic[product.product_id[1]] = [{'qty': product.order_qty}]
+    	    					  		gr_item_total_dic2[product.product_id[0]] = product.order_qty
+    	    					  	 }
+        	        	    	})
+        	        	    	gr_cust_lst = []
+    	        	    		gr_cust_lst.push({'customer_id': c.id, 'product_lst': product_g_list,
+    	        	    						  'gr_qty': gr_qty})
+    	        	    		gr_order_dict[c.name] = gr_cust_lst
+    	        	    		product_g_list = []
+    	        	    	}
+    	        	    	else{
+    	        	    		gr_cust_lst = []
+    	        	    		gr_cust_lst.push({'customer_id': c.id, 'product_lst': gr_product_lst})
+    	        	    		gr_order_dict[c.name] = gr_cust_lst
+    	        	    	}
+    	        	    })
+    	    		})
+    	    	}
+    	    	else{
+    	    		gr_cust_lst = []
+    	    		gr_cust_lst.push({'customer_id': c.id, 'product_lst': gr_product_lst})
+    	    		gr_order_dict[c.name] = gr_cust_lst
+    	    	}
+    	    })
+    		
+    	})
+    });
+
+    instance.web.client_actions.add('grains.order.homepage', 'instance.web_manikarnika.gr_order_action');
+    instance.web_manikarnika.gr_order_action = instance.web.Widget.extend({
+    	events: {
+    		'click #edit': 'input_edit_click',
+    		'click #save': 'input_save_click',
+    	},
+    	template: "GriansOrders",
+        init: function(parent, name) {
+            this._super(parent);
+            var self = this;
+            this.gr_product_lst = gr_product_lst;
+            this.gr_order_dict = gr_order_dict
+            this.gr_item_total = gr_item_total_dic
+        },
+        start: function() {
+        },
+        input_edit_click : function(ev)
+        {
+        	var $action = $(ev.currentTarget);
+        	$action.parent().parent().find('input').attr("readonly", false)
+        	$action.css('visibility', 'hidden')
+        	$action.parent().parent().find('#save').css('visibility', 'visible')
+        },
+        input_save_click: function(ev)
+        {
+        	ev.preventDefault();
+        	var $action = $(ev.currentTarget);
+        	order_dic = {'grain': {}}
+        	view = {}
+        	list_pro = []
+        	$action.parent().parent().find('td').each(function(){
+        		if($(this).find("input")){
+        			if($(this).find("input").attr('id')){
+        				list_pro.push({'product_id': $(this).find("input").attr('id'),'order_qty':$(this).find("input").val()})
+        			}
+				}
+        	})
+        	view[$action.data('dic')] = list_pro
+        	order_dic['grain'] = view
+        	if(view){
+        		var model = new instance.web.Model("order.tacking");
+            	model.call('order_taking_create',{context: order_dic}).then(function(result){
+            		
+            	});
+        	}
+        	$action.parent().parent().find('input').attr("readonly", 'readonly')
+        	$action.css('visibility', 'hidden')
+        	$action.parent().parent().find('#edit').css('visibility', 'visible')
+        },
+    });
+//  ********************************* Vehicle Alloctaion Interface ***********************************
+    var va_driver_list = []
+    var va_order_dict = {}
+    self.va_customer_dataset = new instance.web.DataSetSearch(self, 'res.partner', {}, [['driver', '=' , 'True']])
+    self.va_customer_dataset.read_slice([], {'domain': []}).done(function(records_cus) {
+    	_.each(records_cus, function(c){
+    		self.va_vehicle_dataset = new instance.web.DataSetSearch(self, 'fleet.vehicle', {}, [['driver_id', '=' , c.id]])
+    	    self.va_vehicle_dataset.read_slice([], {'domain': []}).done(function(records_v) {
+    	    	_.each(records_v, function(v){
+    	    		va_driver_list.push({'driver_nm':c.name, 'driver_id': c.id, 'vehicle_nm': v.name})
+    	    	})
+    	    });
+    	})
+    });
+
+    self.va_comp_dataset = new instance.web.DataSetSearch(self, 'res.company', {}, [['comp_code', 'in' , ['GR','MK']]]);
+    self.va_comp_dataset.read_slice([], {'domain': []}).done(function(records_com) {
+    	_.each(records_com, function(r){
+    		self.va_product_dataset = new instance.web.DataSetSearch(self, 'product.product', {}, [['company_id', '=' , r.id]]);
+    	    self.va_product_dataset.read_slice([], {'domain': []}).done(function(records_pro) {
+    	    	_.each(records_pro, function(p){
+    	    		va_pro_lst = []
+    	    		va_pro_lst.push({'product_id': c.id, 'driver_lst': va_driver_list})
+    	    		va_order_dict[p.name] = va_pro_lst
+    	    	})
+    	    });
+    	})
+    });
+
+    
 };
 
 
